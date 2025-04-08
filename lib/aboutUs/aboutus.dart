d546f4d8c7c11b45250d3b6d104645a3b4373132
Kadyrulan <blazemagma5@gmail.com>
2025-04-08 17:26:59 +0500

    changed directories and minor fixes

--- lib/aboutUs/aboutus.dart
+++ lib/aboutUs/aboutus.dart
@@ -18,119 +18,76 @@ class AboutPage extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              AboutCard(
+                title: "About Recipe Recorder App",
+                content:
+                    "Recipe Recorder App helps users efficiently save and manage their favorite recipes. "
+                    "With an integrated database, users can securely store and access recipes anytime. "
+                    "The app also provides cooking tips and suggestions to enhance your culinary skills.",
+              ),
+              const SizedBox(height: 20),
+              AboutCard(
+                title: "Developers",
+                content:
+                    "Developed by:\n\nTanatkanov Kadyrulan\nAsanali Ashimov\nZholaman Yerzhan",
+              ),
+              const SizedBox(height: 20),
+              AboutCard(
+                title: "Course Details",
+                content:
+                    "Course: Crossplatform Development, Astana IT University\n"
+                    "Mentor: Assistant Professor Abzal Kyzyrkanov",
+                isItalic: true,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class AboutCard extends StatelessWidget {
+  final String title;
+  final String content;
+  final bool isItalic;
+
+  const AboutCard({
+    super.key,
+    required this.title,
+    required this.content,
+    this.isItalic = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.grey[900],
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Container(
-              constraints: BoxConstraints(minHeight: 200),
-              child: Card(
-                color: Colors.grey[900],
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text(
-                        "About Recipe Recorder App",
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.amber,
-                        ),
-                      ),
-                      const SizedBox(height: 10),
-                      const Text(
-                        "Recipe Recorder App helps users efficiently save and manage their favorite recipes. "
-                        "With an integrated database, users can securely store and access recipes anytime. "
-                        "The app also provides cooking tips and suggestions to enhance your culinary skills.",
-                        style: TextStyle(fontSize: 16, color: Colors.white70),
-                      ),
-                    ],
-                  ),
-                ),
+            Text(
+              title,
+              style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.amber,
               ),
             ),
-            const SizedBox(height: 20),
-            Container(
-              constraints: BoxConstraints(minHeight: 200),
-              child: Card(
-                color: Colors.grey[900],
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text(
-                        "Developers",
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.amber,
-                        ),
-                      ),
-                      const SizedBox(height: 20),
-                      const Text(
-                        "Developed by:",
-                        style: TextStyle(fontSize: 16, color: Colors.white70),
-                      ),
-                      const SizedBox(height: 20),
-                      const Text(
-                        "Tanatkanov Kadyrulan\nAsanali Ashimov\nZholaman Yerzhan",
-                        style: TextStyle(fontSize: 16, color: Colors.white),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            const SizedBox(height: 20),
-            Container(
-              constraints: BoxConstraints(minHeight: 200),
-              child: Card(
-                color: Colors.grey[900],
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text(
-                        "Course Details",
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.amber,
-                        ),
-                      ),
-                      const SizedBox(height: 10),
-                      const Text(
-                        "Course: Crossplatform Development, Astana IT University",
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.white70,
-                          fontStyle: FontStyle.italic,
-                        ),
-                      ),
-                      const SizedBox(height: 5),
-                      const Text(
-                        "Mentor: Assistant Professor Abzal Kyzyrkanov",
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.white70,
-                          fontStyle: FontStyle.italic,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
+            const SizedBox(height: 10),
+            Text(
+              content,
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.white70,
+                fontStyle: isItalic ? FontStyle.italic : FontStyle.normal,
               ),
             ),
           ],
